b52d7c202a6950e21a8bb5ce089382fd90631081
kkamara <kamaracomputers@gmail.com>
2023-09-19 13:47:48 +0100

    Add deleting a Task route

--- app/Http/Controllers/API/V1/TaskController.php
+++ app/Http/Controllers/API/V1/TaskController.php
@@ -61,6 +61,8 @@ class TaskController extends Controller
      */
     public function destroy(Task $task)
     {
-        //
+        $task->delete();
+
+        return response()->noContent();
     }
 }
